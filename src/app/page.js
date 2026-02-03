d3ba51fa593d1b94b17f4b659d09efb3c19e8fc5
BNMI India <bnmiindia1@gmail.com>
2026-02-03 15:43:37 +0530

    Initial commit

---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Next.js + Tailwind 🚀
+      </h1>
+    </main>
+  )
+}
