e8dd2d9a34817c1fe9f0fa7e45b9eab75221a341
Anup <anupdey9863@gmail.com>
2026-03-12 21:25:45 +0530

    try 1

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,4 +1,4 @@
-import { Client, Databases } from "appwrite";
+import { Client, Databases, Account, Storage, ID } from "appwrite";
 
 const client = new Client();
 
@@ -6,7 +6,17 @@ const endpoint = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT;
 const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID;
 
 if (!endpoint || !projectId) {
-  throw new Error("Appwrite environment variables are not set");
+  throw new Error(
+    "Missing Appwrite env vars: NEXT_PUBLIC_APPWRITE_ENDPOINT and NEXT_PUBLIC_APPWRITE_PROJECT_ID must be set"
+  );
 }
 
-client.setEndpoint(endpoint).setProject(projectId);
+client
+  .setEndpoint(endpoint)
+  .setProject(projectId);
+
+export const databases = new Databases(client);
+export const account = new Account(client);
+export const storage = new Storage(client);
+
+export { ID };
